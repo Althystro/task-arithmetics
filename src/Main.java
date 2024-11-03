1179cec7b6f2dada489c5929868ad9f65882d082
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-11-03 11:08:58 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+
+        int amountKWD = 86;
+    int amountEuro = 203;
+        double kwdToUsd = amountKWD *3.25;
+        double usdToKwd = kwdToUsd /3.25;
+        double euroToKwd = amountEuro/3.33;
+        System.out.println(amountKWD +" KWD = "+ kwdToUsd +" USD");
+
+
+        System.out.println(amountEuro +" Euro = "+ euroToKwd +" KWD");
+
         System.out.println("Hello world!");
     }
 }
